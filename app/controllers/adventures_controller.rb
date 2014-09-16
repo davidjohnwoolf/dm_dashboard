8740a5d2607f0522e61fed19b839447e8e4e43f9
David Woolf <davidjohnwoolf@gmail.com>
2014-09-15 21:19:17 -0600

    create crud actions for adventure controller

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,2 +1,53 @@
 class AdventuresController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_adventure, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @adventures = Adventure.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @adventure = Adventure.new
+  end
+
+  def create
+    @adventure = Player.new(adventure_params)
+    @adventure.user_id = current_user.id
+    if @adventure.save
+      redirect_to adventures_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @adventure.update_attributes(adventure_params)
+      redirect_to adventure_path(@adventure.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @adventure.destroy
+    redirect_to adventures_url
+  end
+
+  private
+
+  def adventure_params
+    params.require(:adventure).permit(:title, :description, :completed)
+  end
+
+  def find_adventure
+    @adventure = Adventure.find(params[:id])
+  end
 end
