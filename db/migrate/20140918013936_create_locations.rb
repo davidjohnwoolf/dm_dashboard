1e9112d16daa58ce718a537eff8d8633e7a613ea
David Woolf <davidjohnwoolf@gmail.com>
2014-09-17 19:42:56 -0600

    add belongs to user and index for user_id to location migration

--- db/migrate/20140918013936_create_locations.rb
+++ db/migrate/20140918013936_create_locations.rb
@@ -1,8 +1,9 @@
 class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
-      t.string :title
-      t.text :description
+      t.belongs_to :user, index: true
+      t.string :title, null: false
+      t.text :description, null: false
 
       t.timestamps
     end
