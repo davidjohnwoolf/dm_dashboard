53a1443c3d74582323bdfeff312fa9ebdddacc87
David Woolf <davidjohnwoolf@gmail.com>
2014-09-18 02:30:48 -0600

    remove model comments about validations

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -1,11 +1,8 @@
 class Adventure < ActiveRecord::Base
   belongs_to :user
-  #validation
   has_many :encounters
   has_many :notes, as: :notable
-
   validates :title, :description, :completed, presence: true
-
   accepts_nested_attributes_for :encounters, :allow_destroy => true
   accepts_nested_attributes_for :notes, :allow_destroy => true
 end
