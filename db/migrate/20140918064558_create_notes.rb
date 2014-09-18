ff0d2dab1c0bf7a8b26a04ee4724a6e2cab49326
David Woolf <davidjohnwoolf@gmail.com>
2014-09-18 00:57:18 -0600

    add plymorphic notes migration attributes

--- db/migrate/20140918064558_create_notes.rb
+++ db/migrate/20140918064558_create_notes.rb
@@ -1,6 +1,9 @@
 class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
+      t.belongs_to :notable, polymorphic: true
+      t.string :title, null: false
+      t.text :text, null: false
 
       t.timestamps
     end
