4c048b81c9005695f2ac201e2e527ec24212c685
David Woolf <davidjohnwoolf@gmail.com>
2014-09-17 19:45:03 -0600

    generate location controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140918005816) do
+ActiveRecord::Schema.define(version: 20140918013936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 20140918005816) do
   end
 
   add_index "encounters", ["adventure_id"], name: "index_encounters_on_adventure_id", using: :btree
+
+  create_table "locations", force: true do |t|
+    t.integer  "user_id"
+    t.string   "title",       null: false
+    t.text     "description", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "locations", ["user_id"], name: "index_locations_on_user_id", using: :btree
 
   create_table "npcs", force: true do |t|
     t.integer  "user_id"
