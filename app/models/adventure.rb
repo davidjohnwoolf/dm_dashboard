d2e11ec297eadd0f9b89e5791215d115ca9ab663
David Woolf <davidjohnwoolf@gmail.com>
2014-09-22 18:51:32 -0600

    remove validation for completed attribute in adventure model

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -2,7 +2,7 @@ class Adventure < ActiveRecord::Base
   belongs_to :user
   has_many :encounters
   has_many :notes, as: :notable
-  validates :title, :description, :completed, presence: true
+  validates :title, :description, presence: true
   accepts_nested_attributes_for :encounters, :allow_destroy => true
   accepts_nested_attributes_for :notes, :allow_destroy => true
 end
