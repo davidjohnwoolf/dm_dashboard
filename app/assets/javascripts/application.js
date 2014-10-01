e9c274181c0ff1837a4b544f79549d79320245b8
David Woolf <davidjohnwoolf@gmail.com>
2014-09-30 22:07:08 -0600

    add js functionality to dashboard tab system

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,14 +5,28 @@
 //= require_tree .
 
 $(document).ready(function() {
+
   $('.encounter').hide();
+
   $('h4.encounter-header').on('click', function() {
     $(this).next().toggle();
   });
-  
-  var maxHeight = 0;
-  $('.dashboard').each(function(){
-     if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
-  });
-  $('.dashboard').height(maxHeight);
-})
+
+  $('.dashboard').hide();
+  $('.players-dash').show();
+
+// site index js
+
+  function dashControl() {
+    $('.dashboard').hide();
+    $('.' + $(this).attr('class') + '-dash').show();
+  };
+
+  function tabClick() {
+    $('.dash-menu ul li').css('backgroundColor', '#999');
+    $(this).css('backgroundColor', '#fff');
+  }
+
+  $('.dash-menu ul li').on('click', tabClick);
+  $('.dash-menu ul li').on('click', dashControl);
+});
